0f3c8bf25c50d939eb6ffef304931e0b9c04d307
Helmer Davila <helmer290192@hotmail.com>
2015-10-30 23:53:31 -0500

    Hace llamadas ajax, solo si existe el elemento por ID

--- public/build/js/graphs.js
+++ public/build/js/graphs.js
@@ -1,22 +1,29 @@
 $(function(){
     // obtener los valores de /api/two-top
-    $.get('/api/two-top', function(data){
-        Morris.Line({
-            element: 'firstchart',
-            data: data.result,
-            lineColors : ['#dd4b39', '#f39c12'],
-            xkey: 'month',
-            ykeys: ['first', 'second'],
-            labels: data.names
-        });
-    });
 
-    // obtener los valores desde /api/all-diseases
-    $.get('/api/all-diseases', function(data){
-        Morris.Donut({
-            element: 'firstdonut',
-            data: data,
-            colors: ["#3c8dbc", "#f56954", "#00a65a"]
-        });
-    });
+    if ($('#firstchart').length) {
+        $.get('/api/two-top', function(data){
+            Morris.Line({
+                element: 'firstchart',
+                data: data.result,
+                lineColors : ['#dd4b39', '#f39c12'],
+                xkey: 'month',
+                ykeys: ['first', 'second'],
+                labels: data.names
+            });
+        }); 
+    };
+
+    if ($('#firstdonut').length) {
+        // obtener los valores desde /api/all-diseases
+        $.get('/api/all-diseases', function(data){
+            Morris.Donut({
+                element: 'firstdonut',
+                data: data,
+                colors: ["#3c8dbc", "#f56954", "#00a65a"]
+            });
+        }); 
+    };
+
+
 });
