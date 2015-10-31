ffb98fb5e846b226e0c4b97c8a511f108208fb3a
Helmer Davila <helmer290192@hotmail.com>
2015-10-30 23:56:59 -0500

    Relacion Departamento tiene muchos diagnosticos a traves de usuario

--- app/Models/State.php
+++ app/Models/State.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+    /**
+     * Relationships
+     */
+    public function diagnostics()
+    {
+        return $this->hasManyThrough(Diagnostic::class, User::class, 'state_id', 'user_id');
+    }
 }
