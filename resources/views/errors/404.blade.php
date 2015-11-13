93a51fce9d1ad50d812b30dc2c5bb50611f4f50a
Helmer Davila <helmer290192@hotmail.com>
2015-11-13 11:16:41 -0500

    Pagina error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,43 @@
-<h1>ERROR 404, NADA POR AQUÍ</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Mi Medico</title>
+        {{-- Tell the browser to be responsive to screen width --}}
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        {{-- Font Awesome --}}
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        {{-- Ionicons --}}
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        {{-- Compiled scripts --}}
+        <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+
+        {{-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --}}
+        {{-- WARNING: Respond.js doesn't work if you view the page via file:// --}}
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="hold-transition">
+        <div class="wrapper">
+            <!-- Main content -->
+            <section class="content">
+                <div class="error-page">
+                    <h2 class="headline text-yellow"> 404</h2>
+                    <div class="error-content">
+                        <h3><i class="fa fa-warning text-yellow"></i> ¡Uy! Página no Encontrada.</h3>
+                        <p>
+                            La página que buscabas no existe.
+                            Puedes <a href="{{ route('showLogin') }}">regresar al inicio</a>.
+                        </p>
+                    </div><!-- /.error-content -->
+                </div><!-- /.error-page -->
+            </section><!-- /.content -->
+        </div><!-- /.content-wrapper -->
+
+        {{-- Compiled scripts --}}
+        <script src="{{ elixir('js/all.js') }}"></script>
+    </body>
+</html>
